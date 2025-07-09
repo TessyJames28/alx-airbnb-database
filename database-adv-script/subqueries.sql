45eae4fb3dc989e437014b2ee6e397e1a0d5ea3c
Tessyjames <tessyjames28@gmail.com>
2025-07-09 22:59:38 +0100

    Used AVG in subqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -7,7 +7,8 @@ SELECT name
 FROM Property
 WHERE property_id IN (
     SELECT property_id FROM Review
-    WHERE rating > 4.0
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
 );
 
 -- Write a correlated subquery to find users who have
